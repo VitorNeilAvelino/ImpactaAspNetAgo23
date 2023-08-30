6262a51caf5167d8080adaffd5392c8e0637b100
Vitor Neil Avelino <avelino.vitor@gmail.com>
2023-08-29 22:43:55 -0300

    Adicionar arquivos de projeto.

---
+++ AspNet.Capitulo02.Razor/Models/ComentarioViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AspNet.Capitulo02.Razor.Models
+{
+    public class ComentarioViewModel
+    {
+        public DateTime Data { get; set; }
+        public string Nome { get; set; }
+        public string Comentario { get; set; }
+    }
+}
