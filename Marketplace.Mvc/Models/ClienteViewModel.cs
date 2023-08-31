7bc739811633e57a9186bd38a5a0d58494c7a926
Vitor Neil Avelino <avelino.vitor@gmail.com>
2023-08-30 22:36:09 -0300

    Marketplace.Mvc

---
+++ Marketplace.Mvc/Models/ClienteViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Marketplace.Mvc.Models
+{
+    public class ClienteViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Documento { get; set; }
+
+        [Required] 
+        public string Nome { get; set; }
+
+        [Required] 
+        public string Telefone { get; set; }
+
+        [Required] 
+        public string Email { get; set; }
+    }
+}
