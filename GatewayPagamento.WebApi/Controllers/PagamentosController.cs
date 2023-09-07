003fd19a7096edc2c0eda62c133fee37679927aa
Vitor Neil Avelino <avelino.vitor@gmail.com>
2023-09-06 22:35:55 -0300

    PagamentoGetViewModel

---
+++ GatewayPagamento.WebApi/Controllers/PagamentosController.cs
@@ -0,0 +1,43 @@
+﻿using GatewayPagamento.Dominio.Interfaces;
+using GatewayPagamento.Repositorios.SqlServer.CodeFirst;
+using GatewayPagamento.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GatewayPagamento.WebApi.Controllers
+{
+    public class PagamentosController : ApiController
+    {
+        private readonly IPagamentoRepositorio pagamentoRepositorio = new PagamentoRepositorio();
+
+        public IEnumerable<PagamentoGetViewModel> Get(Guid guidCartao)
+        {
+            return PagamentoGetViewModel.Mapear(pagamentoRepositorio.Selecionar(guidCartao));
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ GatewayPagamento.WebApi/Models/PagamentoGetViewModel.cs
@@ -0,0 +1,51 @@
+﻿using GatewayPagamento.Dominio.Entidades;
+using GatewayPagamento.WebApi.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Web.Services.Protocols;
+
+namespace GatewayPagamento.WebApi.Models
+{
+    public class PagamentoGetViewModel
+    {
+        public int Id { get; set; }
+        public string MascaraCartao { get; set; }
+        public string NumeroPedido { get; set; }
+        public DateTime Data { get; set; }
+        public decimal Valor { get; set; }
+        public int Status { get; set; }
+        public string DescricaoStatus { get; set; }
+
+        internal static IEnumerable<PagamentoGetViewModel> Mapear(List<Pagamento> pagamentos)
+        {
+            var viewModels = new List<PagamentoGetViewModel>();
+
+            foreach (var pagamento in pagamentos)
+            {
+                viewModels.Add(Mapear(pagamento));
+            }
+
+            return viewModels;
+        }
+
+        private static PagamentoGetViewModel Mapear(Pagamento pagamento)
+        {
+            var viewModel = new PagamentoGetViewModel();
+
+            viewModel.Id = pagamento.Id;
+            viewModel.NumeroPedido = pagamento.NumeroPedido;
+            viewModel.Data = pagamento.Data;
+            viewModel.Valor = pagamento.Valor;
+
+            var numeroCartao = pagamento.Cartao.Numero;
+
+            viewModel.MascaraCartao = $"{numeroCartao.Substring(0, 6)}...{numeroCartao.Substring(numeroCartao.Length - 4)}";
+
+            viewModel.Status = (int)pagamento.Status;
+            viewModel.DescricaoStatus = pagamento.Status.ObterDescricao();
+            
+            return viewModel;
+        }
+    }
+}
