a7bd691873076f27de225d67438c7af12c246b49
Vitor Neil Avelino <avelino.vitor@gmail.com>
2023-09-12 21:02:02 -0300

    Repositório para HTTP - pagamento

---
+++ Marketplace.Repositorios.Http/PagamentoRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using Marketplace.Repositorios.Http.Requests;
+using Marketplace.Repositorios.Http.Responses;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Marketplace.Repositorios.Http
+{
+    public class PagamentoRepositorio
+    {
+        private readonly HttpClient httpClient = new HttpClient();
+        private const string caminho = "pagamentos";
+
+        public PagamentoRepositorio(string baseAddress)
+        {
+            httpClient.BaseAddress = new Uri(baseAddress.Trim('/') + '/');
+        }
+
+        /// <summary>
+        /// Obtém a fatura do cartão.
+        /// </summary>
+        /// <param name="guidCartao">Guid do cartão.</param>
+        public async Task<List<PagamentoResponse>> GetByCartao(Guid guidCartao)
+        {
+            using (var resposta = await httpClient.GetAsync($"{caminho}/cartao/{guidCartao}"))
+            {
+                return await resposta.Content.ReadAsAsync<List<PagamentoResponse>>();
+            }
+        }
+
+        public async Task<PagamentoResponse> Post(PagamentoRequest pagamento)
+        {
+            using (var resposta = await httpClient.PostAsJsonAsync(caminho, pagamento))
+            {
+                return await resposta.Content.ReadAsAsync<PagamentoResponse>();
+            }
+        }
+    }
+}
---
+++ Marketplace.Repositorios.Http/Requests/PagamentoRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Marketplace.Repositorios.Http.Requests
+{
+    public class PagamentoRequest
+    {
+        public string NumeroPedido { get; set; }
+        public DateTime Data { get; set; }
+        public decimal Valor { get; set; }
+        public string NumeroCartao { get; set; }
+    }
+}
