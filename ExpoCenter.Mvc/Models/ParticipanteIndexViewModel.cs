cb70dec6424565110d26ac62fdfe65221e4ae481
Vitor Neil Avelino <avelino.vitor@gmail.com>
2023-09-14 22:44:06 -0300

    View-model de Index

---
+++ ExpoCenter.Mvc/Models/ParticipanteIndexViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpoCenter.Mvc.Models
+{
+    public class ParticipanteIndexViewModel
+    {
+        private string cpf;
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        [Display(Name = "CPF")]
+        public string Cpf 
+        { 
+            get => long.Parse(cpf).ToString(@"000\.000\.000-00") ; 
+            set => cpf = value; 
+        }
+        
+        [Display(Name = "Nascimento")]
+        [DataType(DataType.Date)]
+        public DateTime DataNascimento { get; set; }
+    }
+}
